92aa16e436f8f749c4e1db1c95317eab12e52750
lavaman2911 <168592300+lavaman2911@users.noreply.github.com>
2026-09-23 22:19:10 +0200

    LoRa: add baseband image tags

--- firmware/common/spi_image.hpp
+++ firmware/common/spi_image.hpp
@@ -137,6 +137,8 @@ constexpr image_tag_t image_tag_rttytx{'P', 'R', 'T', 'T'};
 constexpr image_tag_t image_tag_tonedetect{'P', 'T', 'N', 'E'};
 constexpr image_tag_t image_tag_tetrarx{'P', 'T', 'E', 'T'};
 
+constexpr image_tag_t image_tag_lora_rx{'P', 'L', 'O', 'R'};
+constexpr image_tag_t image_tag_lora_tx{'P', 'L', 'O', 'T'};
 constexpr image_tag_t image_tag_noop{'P', 'N', 'O', 'P'};
 
 constexpr image_tag_t image_tag_hackrf{'H', 'R', 'F', '1'};
